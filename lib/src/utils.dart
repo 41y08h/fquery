bbc1cf6fe8083453a39a209ba7dbf4a0d4b1f8cb
Piyush <piyush41y08h@gmail.com>
2022-07-03 20:20:36 +0530

    remove unused file

--- lib/src/utils.dart
+++
@@ -1,6 +0,0 @@
-void enforeTypes(String name, dynamic value, List<Type> types) {
-  if (types.contains(value.runtimeType)) return;
-  throw ArgumentError(
-    '$name must be one of the following types: ${types.join(', ')}',
-  );
-}
