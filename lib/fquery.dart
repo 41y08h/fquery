a648dffe7df0cdc37f9f6c463e3c014b51002f04
Piyush <piyush41y08h@gmail.com>
2022-07-03 18:18:05 +0530

    fix exports

--- lib/fquery.dart
+++ lib/fquery.dart
@@ -4,4 +4,4 @@ export 'src/query_client.dart';
 export 'src/query_client_provider.dart';
 export 'src/hooks/use_query.dart';
 export 'src/hooks/use_query_client.dart';
-export 'src/query.dart' show QueryOptions, RefetchOnMount;
+export 'src/query.dart' show RefetchOnMount, QueryStatus;
