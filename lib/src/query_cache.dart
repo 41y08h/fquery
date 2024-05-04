2ec6b6d338857878074d5b948e1b96c39daf4862
Piyush <piyush41y08h@gmail.com>
2024-05-04 14:12:34 +0530

    extract type to `QueriesMap` typedef

--- lib/src/query_cache.dart
+++ lib/src/query_cache.dart
@@ -2,9 +2,11 @@ import 'package:fast_immutable_collections/fast_immutable_collections.dart';
 import 'package:fquery/src/query.dart';
 import 'package:fquery/src/query_client.dart';
 
+typedef QueriesMap = Map<IList<dynamic>, Query>;
+
 class QueryCache {
-  final Map<IList<dynamic>, Query> _queries = {};
-  Map<IList<dynamic>, Query> get queries => _queries;
+  final QueriesMap _queries = {};
+  QueriesMap get queries => _queries;
 
   Query<TData, TError>? get<TData, TError>(QueryKey queryKey) {
     return _queries[queryKey.lock] as Query<TData, TError>?;
@@ -21,10 +23,12 @@ class QueryCache {
   /// Returns a query identified by the query key.
   /// If it doesn't exist already,
   /// creates a new one and adds it to the cache.
-  Query<TData, TError> build<TData, TError>(
-      {required QueryKey queryKey, required QueryClient client}) {
-    var query = get<TData, TError>(queryKey);
-    query ??= Query(client: client, key: queryKey);
+  Query<TData, TError> build<TData, TError>({
+    required QueryKey queryKey,
+    required QueryClient client,
+  }) {
+    var query =
+        get<TData, TError>(queryKey) ?? Query(client: client, key: queryKey);
     add(queryKey, query);
     return query;
   }
