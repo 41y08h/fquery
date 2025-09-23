63196a90ee0db5b31a1b75f7d4fc6842debfdb8e
Piyush <piyush41y08h@gmail.com>
2025-09-23 23:21:13 +0530

    rewrite queries builder without hooks

--- lib/src/widgets/queries_builder.dart
+++ lib/src/widgets/queries_builder.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/widgets.dart';
-import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:fquery/fquery.dart';
+import 'package:fquery/src/queries_observer.dart';
 
-/// A Builder widget which uses [useQueries] internally
-class QueriesBuilder<TData, TError extends Exception> extends HookWidget {
+/// Builder widget for multiple queries
+class QueriesBuilder<TData, TError extends Exception> extends StatefulWidget {
   /// The builder function which recevies the [UseQueryResult] along with the [BuildContext]
   final Widget Function(BuildContext, List<UseQueryResult<TData, TError>>)
       builder;
@@ -19,13 +19,66 @@ class QueriesBuilder<TData, TError extends Exception> extends HookWidget {
   });
 
   @override
-  Widget build(BuildContext context) {
-    final queries = useQueries<TData, TError>(
-      options,
-    );
+  State<QueriesBuilder<TData, TError>> createState() =>
+      _QueriesBuilderState<TData, TError>();
+}
 
-    return Builder(builder: (context) {
-      return builder(context, queries);
+class _QueriesBuilderState<TData, TError extends Exception>
+    extends State<QueriesBuilder<TData, TError>> {
+  late final client = QueryClient.of(context);
+  late QueriesObserver<TData, TError> observer;
+
+  // Initialization of the observer
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+
+    setState(() {
+      observer = QueriesObserver<TData, TError>(client: client);
+      observer.setOptions(widget.options);
     });
+  }
+
+  @override
+  void didUpdateWidget(covariant QueriesBuilder<TData, TError> oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    observer.setOptions(widget.options);
+  }
+
+  @override
+  void dispose() {
+    observer.destroy();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ListenableBuilder(
+      listenable: observer,
+      builder: (context, _) {
+        // Always build intermediate value inside builder
+        // to update on every change
+        final queries = observer.observers
+            .map(
+              (observer) => UseQueryResult(
+                data: observer.query.state.data,
+                dataUpdatedAt: observer.query.state.dataUpdatedAt,
+                error: observer.query.state.error,
+                errorUpdatedAt: observer.query.state.errorUpdatedAt,
+                isError: observer.query.state.isError,
+                isLoading: observer.query.state.isLoading,
+                isFetching: observer.query.state.isFetching,
+                isSuccess: observer.query.state.isSuccess,
+                status: observer.query.state.status,
+                refetch: observer.fetch,
+                isInvalidated: observer.query.state.isInvalidated,
+                isRefetchError: observer.query.state.isRefetchError,
+              ),
+            )
+            .toList();
+
+        return widget.builder(context, queries);
+      },
+    );
   }
 }
