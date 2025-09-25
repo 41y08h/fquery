e9c6ba58cb67f0c65d89050442d4a6fd5db57469
Piyush <piyush41y08h@gmail.com>
2025-09-25 11:52:27 +0530

    create queries instance

---
+++ lib/src/instances/queries_instance.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/widgets.dart';
+import 'package:fquery/fquery.dart';
+import 'package:fquery/src/models/query_result.dart';
+import 'package:fquery/src/observers/queries_observer.dart';
+
+class QueriesInstance {
+  static List<QueryResult<TData, TError>> of<TData, TError extends Exception>(
+      BuildContext context, List<QueryOptions<TData, TError>> options) {
+    final client = QueryClient.of(context);
+    final observer = QueriesObserver<TData, TError>(
+      client: client,
+    );
+    final queries = observer.observers
+        .map(
+          (observer) => QueryResult(
+            data: observer.query.data,
+            dataUpdatedAt: observer.query.dataUpdatedAt,
+            error: observer.query.error,
+            errorUpdatedAt: observer.query.errorUpdatedAt,
+            isError: observer.query.isError,
+            isLoading: observer.query.isLoading,
+            isFetching: observer.query.isFetching,
+            isSuccess: observer.query.isSuccess,
+            status: observer.query.status,
+            refetch: observer.fetch,
+            isInvalidated: observer.query.isInvalidated,
+            isRefetchError: observer.query.isRefetchError,
+          ),
+        )
+        .toList();
+
+    return queries;
+  }
+}
